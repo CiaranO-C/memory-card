811ae85f53425a61d919df7d3182e3ccc4a89c89
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-06-11 12:20:10 +0100

    create pokemon data file. write function to fetch pokemon images from pokeAPI and update images state

---
+++ src/pokeData.js
@@ -0,0 +1,16 @@
+const pokeNames = [
+  "pikachu",
+  "charmander",
+  "bulbasaur",
+  "squirtle",
+  "jigglypuff",
+  "meowth",
+  "psyduck",
+  "snorlax",
+  "eevee",
+  "gengar",
+  "machamp",
+  "mewtwo",
+];
+
+export default pokeNames;
